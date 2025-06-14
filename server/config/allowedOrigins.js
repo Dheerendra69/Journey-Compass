fb3dc3d51aca73e07cafe4849d2cdbc7e265092d
Dheerendra <118679439+Dheerendra69@users.noreply.github.com>
2025-06-14 20:07:57 +0530

    Update allowedOrigins.js

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -3,7 +3,8 @@ const allowedOrigins = [
     'http://localhost:5173',
     'http://localhost:4200',
     'https://journey-compass.netlify.app',
-    'http://localhost:3000'
+    'http://localhost:3000',
+    'http://localhost:5174',
 
 ];
 
