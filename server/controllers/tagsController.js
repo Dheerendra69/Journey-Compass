f40e8ffcae93daecf1400b849c5bc182745bea0f
Dheerendra <118679439+Dheerendra69@users.noreply.github.com>
2025-07-19 20:39:34 +0530

    Update tagsController.js

--- server/controllers/tagsController.js
+++ server/controllers/tagsController.js
@@ -1,18 +1,19 @@
 const Article = require("../models/Article");
 
 
-
 const getTags = async (req, res) => {
-
-    //return distinct tags 
-
-    const tags = await Article.find().distinct('tagList').exec();
+  try {
+    const tags = await Article.find().distinct("tagList").exec();
+    const limitedTags = tags.slice(0, 15); // Get only the first 15 tags
 
     res.status(200).json({
-        tags:tags
+      tags: limitedTags,
     });
-
+  } catch (error) {
+    res.status(500).json({ message: "Failed to fetch tags", error });
+  }
 };
+
 
 
 module.exports = {
