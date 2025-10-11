b86f209baa9da9f07b172437083120fd674d36b4
Dheerendra <118679439+Dheerendra69@users.noreply.github.com>
2025-10-11 23:33:13 +0530

    Update coldStarts.js

--- src/utils/coldStarts.js
+++ src/utils/coldStarts.js
@@ -1,7 +1,8 @@
+// this is a personal file to start backend of all my projects. you can delete this file and the useEffect from ../App.js. Still the website will work fine
 const urls = [
-  "https://event-booking-system-tqf3.onrender.com/api/wake-up",
-  "https://stationaryheaven-final.onrender.com/api/wake-up",
   "https://blogging-website-x3hj.onrender.com/api/wake-up",
+  "https://event-booking-system-tqf3.onrender.com/api/wake-up",
+  "https://stationery-heaven-v2.onrender.com/api/wake-up",
   //   "http://localhost:4000/api/wake-up",
 ];
 
