ee91f5c97301a2f98d7bae0f20761bc12c3f852a
Dheerendra <118679439+Dheerendra69@users.noreply.github.com>
2025-10-11 12:07:24 +0530

    Update coldStarts.js

--- src/utils/coldStarts.js
+++ src/utils/coldStarts.js
@@ -1,1 +1,19 @@
+const urls = [
+  "https://event-booking-system-tqf3.onrender.com/api/wake-up",
+  "https://stationaryheaven-final.onrender.com/api/wake-up",
+  "https://blogging-website-x3hj.onrender.com/api/wake-up",
+  //   "http://localhost:4000/api/wake-up",
+];
 
+export const warmUpBackend = async () => {
+  console.log("Warming up backend...");
+  for (const url of urls) {
+    try {
+      const res = await fetch(url);
+    //   console.log(`Pinged: ${url} - ${res.status}`);
+    } catch (err) {
+    //   console.log(`Failed to reach ${url}:`, err.message);
+    }
+  }
+  console.log("Warm-up completed!");
+};
