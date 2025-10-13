0794b55964cdbade8d7c4cb6efd3ae7d7b4b9a77
Dheerendra <118679439+Dheerendra69@users.noreply.github.com>
2025-10-13 18:12:14 +0530

    Update allowedOrigins.js

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,8 +1,7 @@
 const allowedOrigins = [
-    'http://localhost:5173',
-    'http://localhost:5174',
     'https://journey-compass.netlify.app',
     'https://event-bookin.netlify.app',
+    'https://event-bookin-system.vercel.app',
 ];
 
 module.exports = allowedOrigins;
